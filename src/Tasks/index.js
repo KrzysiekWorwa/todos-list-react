22917b4529e9a0823f1c5b32e1e9a505f1b042c3
Krzysztof Worwa <worwak1998@gmail.com>
2025-02-19 14:48:53 +0100

    fixing tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -6,7 +6,7 @@ const Tasks = ({tasks, hideDoneTasks}) => (
             <li className={`tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""}`}
             key={task.id}>
                 <button className="tasks__button tasks__button--toggleDone">{task.done ? "✓" : ""}</button>
-            <span className={`tasks__content{task.done ? " tasks__content--done" : ""}`}>{task.content}</span>
+            <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>{task.content}</span>
             <button className="tasks__button tasks__button--remove">
             ⨉
             </button>
