c3084a54fa2fff77edf357666fe3aa9e7e775de0
Krzysztof Worwa <worwak1998@gmail.com>
2025-02-25 12:24:04 +0100

    adding toggleTaskDone interaction

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,16 +1,16 @@
 import "./style.css";
 
-const Tasks = ({tasks, hideDone, removeTask}) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li className={`tasks__item${task.done && hideDone ? " tasks__item--hidden" : ""}`}
-            key={task.id}>
-                <button className="tasks__button tasks__button--toggleDone">{task.done ? "✓" : ""}</button>
-            <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>{task.content}</span>
-            <button onClick={() => removeTask(task.id)} className="tasks__button tasks__button--remove">
-            ⨉
-            </button>
-                </li>
+                key={task.id}>
+                <button onClick={() => toggleTaskDone(task.id)} className="tasks__button tasks__button--toggleDone">{task.done ? "✓" : ""}</button>
+                <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>{task.content}</span>
+                <button onClick={() => removeTask(task.id)} className="tasks__button tasks__button--remove">
+                    ⨉
+                </button>
+            </li>
         ))}
     </ul>
 );
