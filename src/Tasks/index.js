6db03d3aca4255684a0afcf32e177c1d70721e0c
Krzysztof Worwa <worwak1998@gmail.com>
2025-02-25 11:24:53 +0100

    adding removeTask interaction

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,13 +1,13 @@
 import "./style.css";
 
-const Tasks = ({tasks, hideDone}) => (
+const Tasks = ({tasks, hideDone, removeTask}) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li className={`tasks__item${task.done && hideDone ? " tasks__item--hidden" : ""}`}
             key={task.id}>
                 <button className="tasks__button tasks__button--toggleDone">{task.done ? "✓" : ""}</button>
             <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>{task.content}</span>
-            <button className="tasks__button tasks__button--remove">
+            <button onClick={() => removeTask(task.id)} className="tasks__button tasks__button--remove">
             ⨉
             </button>
                 </li>
