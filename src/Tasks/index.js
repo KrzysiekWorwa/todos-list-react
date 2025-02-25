9c96679802194453e1bea8e69b9ee37f1ab90803
Krzysztof Worwa <worwak1998@gmail.com>
2025-02-25 11:03:13 +0100

    hideDoneTasks renamed to hideDone

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,9 +1,9 @@
 import "./style.css";
 
-const Tasks = ({tasks, hideDoneTasks}) => (
+const Tasks = ({tasks, hideDone}) => (
     <ul className="tasks">
         {tasks.map(task => (
-            <li className={`tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""}`}
+            <li className={`tasks__item${task.done && hideDone ? " tasks__item--hidden" : ""}`}
             key={task.id}>
                 <button className="tasks__button tasks__button--toggleDone">{task.done ? "✓" : ""}</button>
             <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>{task.content}</span>
