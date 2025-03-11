e244a09b41fdb26f0d1b79386e5d2a5e4a7fcdc7
Krzysztof Worwa <worwak1998@gmail.com>
2025-03-11 19:13:49 +0100

    adding styled.js to Section

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,17 +1,18 @@
+import { SectionBody, SectionHeader, Title, Body } from "./styled";
 
-import "./style.css";
 
-const Section = ({title, body, extraHeaderContent}) => (
+const Section = ({ title, body, extraHeaderContent }) => (
 
-<section className="section">
-            <header className="section__header">
-                <h2 className="section__title">{title}</h2>
-                {extraHeaderContent}
-            </header>
-            <div className="section__body">
-                {body}
-            </div>
-        </section>
+    <SectionBody>
+        <SectionHeader>
+            <Title>{title}
+            </Title>
+            {extraHeaderContent}
+        </SectionHeader>
+        <Body>
+            {body}
+        </Body>
+    </SectionBody>
 
 );
 
