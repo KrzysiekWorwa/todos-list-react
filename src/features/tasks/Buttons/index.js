9b0ed1f918d581eec6886e8e130040970429fb8f
Krzysztof Worwa <worwak1998@gmail.com>
2025-03-18 20:26:13 +0100

    adding redux-saga

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,29 +1,34 @@
 import { useSelector, useDispatch } from "react-redux";
 import { ButtonsDiv, Button } from "./styled";
-import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
+import { selectTasks, toggleHideDone, setAllDone, fetchExampleTasks } from "../tasksSlice";
 
 const Buttons = () => {
 
-    const {tasks, hideDone} = useSelector(selectTasks);
+    const { tasks, hideDone } = useSelector(selectTasks);
     const dispatch = useDispatch();
 
     const hasDoneTasks = tasks.some(({ done }) => done);
 
     return (
-    tasks.length > 0 && (
         <ButtonsDiv>
-        <Button onClick={() => dispatch(toggleHideDone())} disabled={!hasDoneTasks} >
-          {hideDone ? "Pokaż" : "Ukryj"} ukończone
-        </Button>
-            <Button
-                onClick={() => dispatch(setAllDone())}
-                disabled={tasks.every(({ done }) => done)}
-            >
-                Ukończ wszystkie
+            <Button onClick={() => dispatch(fetchExampleTasks())}>
+                Pobierz przykładowe zadania
             </Button>
+            {tasks.length > 0 && (
+                <>
+                    <Button onClick={() => dispatch(toggleHideDone())} disabled={!hasDoneTasks} >
+                        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                    </Button>
+                    <Button
+                        onClick={() => dispatch(setAllDone())}
+                        disabled={tasks.every(({ done }) => done)}
+                    >
+                        Ukończ wszystkie
+                    </Button>
+                </>
+            )}
         </ButtonsDiv>
-    )
-);
+    );
 };
 
 
